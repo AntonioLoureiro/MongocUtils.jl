2d1bdcc957580c7090a0b3f53add8e992ac1c41a
kuren <6925634+kuren@users.noreply.github.com>
2021-07-21 11:29:57 +0100

    Create runtests2.jl

---
+++ test/runtests2.jl
@@ -0,0 +1,29 @@
+module X
+    using MongocUtils, Mongoc
+
+    module E
+        using MongocUtils, Mongoc
+
+        struct N
+            x::Int64
+        end
+
+        function a()
+            n = N(1)
+            return Mongoc.BSON(n)
+        end
+    end
+
+    function b()
+        n = E.N(2)
+        return Mongoc.BSON(n)
+    end
+end
+
+n = X.E.a()
+n_st = as_struct(X.E.N, n)
+@test n_st.x == 1
+
+n = X.b()
+n_st = as_struct(X.E.N, n)
+@test n_st.x == 2
