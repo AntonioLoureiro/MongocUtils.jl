078c4271f414bfac2b8bdb0fb7ff8a4e3ac05e3f
AntonioLoureiro <ajose.loureiro@gmail.com>
2021-08-04 10:43:31 +0100

    Update as_struct.jl

--- src/as_struct.jl
+++ src/as_struct.jl
@@ -84,43 +84,53 @@ function as_struct(dt::Type{T} where T,x)
     
     _type=haskey(x,"_type") ? x["_type"] : nothing
     
-    if dt<:Type
-        return str_to_type(get(x,"_value",nothing))
-    elseif dt<:AbstractDict
-        return as_struct(dt,x)
-    elseif dt<:MongocUtils.BSON_PRIMITIVE
-        return x 
-    elseif _type=="Symbol"
-        return Symbol(x["_value"])
-    elseif isconcretetype(dt)
-        return dt([as_struct(fieldtype(dt,k),x[string(k)]) for k in fieldnames(dt)]...)
-    elseif dt==Any   
-        if _type==nothing
-            if x isa AbstractDict
-                return as_struct(Dict,x)
-            else
-                return x
-            end
-        else
-            return as_struct(str_to_type(_type),x)
-        end
-    ## dt is abstract
-    else
-        if _type==nothing
-            types_arr=get_concrete_types(dt)
-            try 
-               for t in types_arr
-                  return  as_struct(t,x)
-               end
-            catch;
-               return x
-            end
-        elseif _type=="Type"
+    try
+        if dt<:Type
             return str_to_type(get(x,"_value",nothing))
+        elseif dt<:AbstractDict
+            return as_struct(dt,x)
+        elseif dt<:MongocUtils.BSON_PRIMITIVE
+            return x 
+        elseif _type=="Symbol"
+            return Symbol(x["_value"])
+        elseif isconcretetype(dt)
+            return dt([as_struct(fieldtype(dt,k),x[string(k)]) for k in fieldnames(dt)]...)
+        elseif dt==Any   
+            if _type==nothing
+                if x isa AbstractDict
+                    return as_struct(Dict,x)
+                else
+                    return x
+                end
+            else
+                return as_struct(str_to_type(_type),x)
+            end
+        ## dt is abstract
         else
-            stored_type=str_to_type(_type)
-            @assert stored_type<:dt "Stored $(string(stored_type)) is not a subtype of abstract type $(string(dt))"
-            return as_struct(stored_type,x)
+            if _type==nothing
+                types_arr=get_concrete_types(dt)
+                try 
+                   for t in types_arr
+                      return  as_struct(t,x)
+                   end
+                catch;
+                   return x
+                end
+            elseif _type=="Type"
+                return str_to_type(get(x,"_value",nothing))
+            else
+                stored_type=str_to_type(_type)
+                @assert stored_type<:dt "Stored $(string(stored_type)) is not a subtype of abstract type $(string(dt))"
+                return as_struct(stored_type,x)
+            end
+        end
+        
+    catch err;
+        if err isa MethodError
+            return error("Probably struct with no default constructor, create a method with the following signature-> function as_struct(dt::Type{$(string(dt))},x)")
+        else
+            return throw(err)
         end
     end
+        
 end    
