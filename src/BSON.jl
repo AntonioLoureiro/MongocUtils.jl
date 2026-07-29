eebf05ab5247a61a54e2f61ac813b224492018e0
AntonioLoureiro <ajose.loureiro@gmail.com>
2026-07-29 16:18:03 +0100

    Replace eval with runtime-generated BSON functions

--- src/BSON.jl
+++ src/BSON.jl
@@ -1,124 +1,110 @@
-
 ## Type
-function Base.setindex!(d::Mongoc.BSON,tv::Type,st::AbstractString)
-    d[st]=Dict("_type"=>"Type","_value"=>string(nameof(tv)))
+function Base.setindex!(d::Mongoc.BSON, tv::Type, st::AbstractString)
+    d[st] = Dict("_type" => "Type", "_value" => string(nameof(tv)))
 end
 
 ## Date
-Base.setindex!(d::Mongoc.BSON,tv::Date,st::String)=d[st]=DateTime(tv)
+Base.setindex!(d::Mongoc.BSON, tv::Date, st::String) = d[st] = DateTime(tv)
 
 ## Enum
-Base.setindex!(d::Mongoc.BSON,tv::Enum,st::String)=d[st]=Int(tv)
-Mongoc.BSON(s::BSON_VALUE_PRIMITIVE)=s
+Base.setindex!(d::Mongoc.BSON, tv::Enum, st::String) = d[st] = Int(tv)
+Mongoc.BSON(s::BSON_VALUE_PRIMITIVE) = s
 
-Mongoc.BSON(s::Enum)=Int(s)
-Mongoc.BSON(s::Symbol)=Dict("_type"=>"Symbol","_value"=>string(s))
+Mongoc.BSON(s::Enum) = Int(s)
+Mongoc.BSON(s::Symbol) = Dict("_type" => "Symbol", "_value" => string(s))
 
-function Base.setindex!(d::Mongoc.BSON,tv::BSON_VALUE_PRIMITIVE,st)
-    d[string(hash(st),base = 62)]=Dict("_k"=>Mongoc.BSON(st),"_v"=>tv)
+function Base.setindex!(d::Mongoc.BSON, tv::BSON_VALUE_PRIMITIVE, st)
+    d[string(hash(st), base = 62)] = Dict("_k" => Mongoc.BSON(st), "_v" => tv)
 end
 
-function Base.setindex!(d::Mongoc.BSON,tv,st)
+function Base.setindex!(d::Mongoc.BSON, tv, st)
     # Important for Dicts with st String but no method already defined for tv
     if st isa AbstractString
-        d[st]=Mongoc.BSON(tv)
+        d[st] = Mongoc.BSON(tv)
     elseif tv isa Vector
-        d[string(hash(st),base = 62)]=Dict("_k"=>Mongoc.BSON(st),"_v"=>map(x->x isa BSON_VALUE_PRIMITIVE ? x : Mongoc.BSON(x),tv))
+        d[string(hash(st), base = 62)] = Dict(
+            "_k" => Mongoc.BSON(st),
+            "_v" => map(x -> x isa BSON_VALUE_PRIMITIVE ? x : Mongoc.BSON(x), tv),
+        )
     else
-        d[string(hash(st),base = 62)]=Dict("_k"=>Mongoc.BSON(st),"_v"=>Mongoc.BSON(tv))
+        d[string(hash(st), base = 62)] = Dict("_k" => Mongoc.BSON(st), "_v" => Mongoc.BSON(tv))
     end
 end
 
-data_type_fullname(dt::DataType)=join(fullname(parentmodule(dt)),".")*"."*string(nameof(dt))
+str_datatype(datatype::DataType) = string(nameof(datatype))
 
-str_datatype(datatype::DataType)=string(nameof(datatype))
-                        
-function bson_expr(datatype::DataType)
-    arr_f=fieldnames(datatype)
-    str_type=str_datatype(datatype)
-    constr_ex_arr=[]
-        for f in arr_f    
-            push!(constr_ex_arr,"\""*string(f)*"\"=>getfield(s,:$f)")   
-        end
-        ## _type
-        push!(constr_ex_arr,"\"_type\"=>\""*str_type*"\"")
-                
-    return Meta.parse("function Mongoc.BSON(s::$(data_type_fullname(datatype))) begin return try Mongoc.BSON("*join(constr_ex_arr,",")*") catch; BSON_fallback(s) end end end")
-    
-end
-
-function bson_setindex_expr(datatype::DataType)
-   return Meta.parse("function Base.setindex!(d::Mongoc.BSON,tv::$(data_type_fullname(datatype)),st::AbstractString) begin d[st]=Mongoc.BSON(tv); return nothing end end")
-end
-
-naiveBSON(s::BSON_PRIMITIVE)=s
-naiveBSON(s::Vector)=naiveBSON.(s)
-naiveBSON(s::Symbol)=Mongoc.BSON("_type"=>"Symbol","_value"=>string(s))
+naiveBSON(s::BSON_PRIMITIVE) = s
+naiveBSON(s::Vector) = naiveBSON.(s)
+naiveBSON(s::Symbol) = Mongoc.BSON("_type" => "Symbol", "_value" => string(s))
 
 function naiveBSON(s)
-    document=Mongoc.BSON()
-    ts=typeof(s)
-    ## _type
-    str_type=str_datatype(ts)
-    document["_type"]=str_type
-    fs=fieldnames(ts)
-    for f in fs
-        v=getfield(s,f)
-        if !(hasmethod(Mongoc.BSON,Tuple{typeof(v)})) || v isa MongocUtils.BSON_PRIMITIVE || v isa AbstractArray
-             document[string(f)]=MongocUtils.naiveBSON(v)
+    document = Mongoc.BSON()
+    ts = typeof(s)
+    document["_type"] = str_datatype(ts)
+
+    for f in fieldnames(ts)
+        v = getfield(s, f)
+        if !hasmethod(Mongoc.BSON, Tuple{typeof(v)}) || v isa MongocUtils.BSON_PRIMITIVE || v isa AbstractArray
+            document[string(f)] = MongocUtils.naiveBSON(v)
         else
-            document[string(f)]=Mongoc.BSON(v)
+            document[string(f)] = Mongoc.BSON(v)
         end
-    end  
-    
+    end
+
     return document
 end
 
-Mongoc.BSON(s)=BSON_fallback(s)
+const BSON_FUNCTIONS = Dict{DataType,Any}()
+const BSON_FUNCTIONS_LOCK = ReentrantLock()
 
-function whereis(mod::Symbol,parent::Module)
-   arr=names(parent,all=true)
-   if isdefined(parent, mod)
-        return parent
-   else
-       for r in arr
-          r in [nameof(parent),:Base,:Core] ? continue : nothing
-          sub=getproperty(parent,r)
-          sub isa Module ? nothing : continue
-          found=whereis(mod,sub)
-          if found!=nothing
-              return found
-          end
-       end
-   end
-    return Main
+function build_bson_function(datatype::DataType)
+    pairs = Any[
+        :($(string(f)) => getfield(s, $(QuoteNode(f))))
+        for f in fieldnames(datatype)
+    ]
+    push!(pairs, :("_type" => $(str_datatype(datatype))))
+
+    bson_call = Expr(:call, :(Mongoc.BSON), pairs...)
+    expression = :(s -> begin
+        try
+            $bson_call
+        catch
+            MongocUtils.naiveBSON(s)
+        end
+    end)
+
+    return RuntimeGeneratedFunctions.RuntimeGeneratedFunction(
+        MongocUtils,
+        MongocUtils,
+        expression,
+    )
 end
 
-MONGOC_NS=Main
-function BSON_fallback(s)
-    ts = typeof(s)
-    if isdefined(MONGOC_NS,:Mongoc)
-        mongo_mod=MONGOC_NS
-    else
-        mongo_mod = whereis(:Mongoc,Main)
-        global MONGOC_NS=mongo_mod
+function bson_function(datatype::DataType)
+    lock(BSON_FUNCTIONS_LOCK) do
+        return get!(BSON_FUNCTIONS, datatype) do
+            build_bson_function(datatype)
+        end
     end
-    
-    if isdefined(MONGOC_NS,:Mongoc)
-        fs=fieldnames(ts)
-           
-        for f in fs
-            tnf=typeof(getfield(s,f))
-            fnf=fieldnames(tnf)
-            if !(hasmethod(setindex!,Tuple{Mongoc.BSON,tnf,String}))
-                Core.eval(mongo_mod,bson_expr(tnf))
-                Core.eval(mongo_mod,bson_setindex_expr(tnf))
+end
+
+Mongoc.BSON(s) = BSON_fallback(s)
+BSON_fallback(s) = bson_function(typeof(s))(s)
+
+function whereis(mod::Symbol, parent::Module)
+    arr = names(parent, all = true)
+    if isdefined(parent, mod)
+        return parent
+    else
+        for r in arr
+            r in [nameof(parent), :Base, :Core] ? continue : nothing
+            sub = getproperty(parent, r)
+            sub isa Module ? nothing : continue
+            found = whereis(mod, sub)
+            if found !== nothing
+                return found
             end
         end
-
-        Core.eval(mongo_mod,bson_expr(ts))
-        Core.eval(mongo_mod,bson_setindex_expr(ts))
     end
-            
-    return naiveBSON(s)
+    return Main
 end
