87d34e6d5f0505f08fd951a35855016b0d5a8e76
AntonioLoureiro <ajose.loureiro@gmail.com>
2026-07-29 16:17:34 +0100

    Initialize RuntimeGeneratedFunctions

--- src/MongocUtils.jl
+++ src/MongocUtils.jl
@@ -1,14 +1,15 @@
 module MongocUtils
 
-using Mongoc,Dates,InteractiveUtils
+using Mongoc, Dates, InteractiveUtils, RuntimeGeneratedFunctions
 import Base.setindex!
 import Mongoc.BSON
 
-export @BSON,@BSON_setindex
+RuntimeGeneratedFunctions.init(@__MODULE__)
+
 export as_struct
 
-const BSON_PRIMITIVE=Union{Mongoc.BSONObjectId,Number,AbstractString,DateTime,Mongoc.BSON,Type,Dict,Mongoc.BSONCode,Date,Vector{UInt8},Nothing,Enum}
-const BSON_VALUE_PRIMITIVE=Union{Mongoc.BSONObjectId,Number,AbstractString,DateTime,Mongoc.BSONCode,Date,Vector{UInt8},Nothing}
+const BSON_PRIMITIVE = Union{Mongoc.BSONObjectId,Number,AbstractString,DateTime,Mongoc.BSON,Type,Dict,Mongoc.BSONCode,Date,Vector{UInt8},Nothing,Enum}
+const BSON_VALUE_PRIMITIVE = Union{Mongoc.BSONObjectId,Number,AbstractString,DateTime,Mongoc.BSONCode,Date,Vector{UInt8},Nothing}
 
 include("BSON.jl")
 include("as_struct.jl")
